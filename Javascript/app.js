8d1417d2ebbfd0597e0a06f1fc8b855fb863dc40
Jana Abdelkader <janaosama1300@gmail.com>
2021-04-04 16:03:27 +0300

    1st attempt

---
+++ Javascript/app.js
@@ -0,0 +1,7 @@
+let username = prompt("what is your name?")
+let age = prompt("How old are you?")
+let isfan = prompt("Are you a Harry Potter fan?")
+let purchese = prompt("Are you willing to purchese the book?")
+
+console.log (username, age, isfan, purchese)
+alert("Welcome, "+username+" fan or not!")
